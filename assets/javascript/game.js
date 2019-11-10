397cd6a85f8325314d877451b89e1a255ce76d70
Brooks Burwell <brooksdburwell@gmail.com>
2019-11-10 14:51:03 -0800

    css updates to game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,13 +5,21 @@ var gamesLost = 0;
 var guessesRemaining = 9;
 var lettersGuessed = [];
 var letterToGuess = [];
+var previousGameLetter = [];
 
 
 // Loading my game and computer guess
 window.onload = function() {
 	var computerChoice = letters[Math.floor(Math.random() * letters.length)];
-	letterToGuess.push(computerChoice);
-	console.log(letterToGuess[0]);
+    letterToGuess.push(computerChoice);
+    console.log(letterToGuess[0]);
+    
+    
+    document.querySelector("#wins").innerHTML = "Wins: " + gamesWon;
+    document.querySelector("#losses").innerHTML = "Losses: " + gamesLost;
+    document.querySelector("#guesses-left").innerHTML = "# of guesses remaining: " + guessesRemaining;
+    document.querySelector("#guesses").innerHTML = "Letters guessed: " + lettersGuessed;
+    document.querySelector("#previous-game").innerHTML = "Previous rounds letter: " + previousGameLetter;
 }
 
 document.onkeyup = function(event) {
@@ -21,15 +29,18 @@ document.onkeyup = function(event) {
     if ((userGuess === letterToGuess[0]) && (guessesRemaining > 0)) {
         gamesWon++;
         guessesRemaining = 9;
+        lettersGuessed = [];
         letterToGuess = [];
+        previousGameLetter = [];
         var computerChoice = letters[Math.floor(Math.random() * letters.length)];
+        previousGameLetter.push(computerChoice);
         letterToGuess.push(computerChoice);
         console.log(letterToGuess[0]);
     }
 
     else if ((userGuess !== letterToGuess[0]) && (guessesRemaining > 0)) {
         guessesRemaining--;
-        lettersGuessed.push(userGuess);
+        lettersGuessed.push(" " + userGuess);
     }
 
     else if (guessesRemaining === 0) {
@@ -37,13 +48,16 @@ document.onkeyup = function(event) {
         guessesRemaining = 9;
         lettersGuessed = [];
         letterToGuess = [];
+        previousGameLetter = [];
         var computerChoice = letters[Math.floor(Math.random() * letters.length)];
+        previousGameLetter.push(computerChoice);
         letterToGuess.push(computerChoice);
         console.log(letterToGuess[0]);
     }
 
     document.querySelector("#wins").innerHTML = "Wins: " + gamesWon;
-    document.querySelector("#losses").innerHTML = "losses: " + gamesLost;
-    document.querySelector("#guesses-left").innerHTML = "Guesses left: " + guessesRemaining;
-    document.querySelector("#guesses").innerHTML = "Guesses so far: " + lettersGuessed;
+    document.querySelector("#losses").innerHTML = "Losses: " + gamesLost;
+    document.querySelector("#guesses-left").innerHTML = "# of guesses remaining: " + guessesRemaining;
+    document.querySelector("#guesses").innerHTML = "Letters guessed: " + lettersGuessed;
+    document.querySelector("#previous-game").innerHTML = "Previous rounds letter: " + previousGameLetter;
 };
